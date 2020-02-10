31c1ec8080741b57812bd472f537c146959efea3
Kishor Kunal <46112041+kunal951990@users.noreply.github.com>
2020-02-10 22:45:02 +0530

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 30 12:36:28 2020
+
+@author: kisku
+"""
+
+"""1 1 2 3 5"""
+
+
+#def fac(n):
+#    if (n==0|n==1):
+#        return(1)
+#    return(fac(n-1)*n)
+#    
+#
+#
+#print(fac(5))
+
+#def fib(n):
+#    if (n<=1):
+#        return(1)
+#    return(fib(n-2)+fib(n-1))
+#    
+#    
+#for i in range(0,8):
+#    print(fib(i))
+
